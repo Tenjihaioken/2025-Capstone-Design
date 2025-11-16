84798933f893e6a98f6ea67787d54d662feb00b0
ty0386 <7635as60@gmail.com>
2025-11-16 19:32:11 +0900

    맵 완성 (지하 정화조실) + 클리어 시 씬 이동 완성

--- Assets/TopDown_AI/Scripts/Trigger_LoadScene.cs
+++ Assets/TopDown_AI/Scripts/Trigger_LoadScene.cs
@@ -14,7 +14,7 @@ public class Trigger_LoadScene : MonoBehaviour {
 	}
 	void OnTriggerEnter(Collider user){
 		if (user.tag == "Player") {
-			Application.LoadLevel("TopDown_Stage2");
+			Application.LoadLevel(sceneName);
 		}
 	}
 }
